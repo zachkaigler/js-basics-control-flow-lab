386f9fd626ba5b5645d611a74379ff6bb815fa06
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-03-15 13:04:38 -0400

    commit tree d2b73f869b868e25a951e8574865e9f5a1933f9e

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideLength){
   // Write your code here!
+  let greeting;
+  if (rideLength <= 400) {
+    greeting = "This one is on me!";
+  } else if (rideLength > 2000 && rideLength <= 2500) {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else if (rideLength > 2500) {
+    greeting = "No can do.";
+  }
+  return greeting;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let greeting;
+  city === "NYC" ? (greeting = "Ok, sounds good.") : (greeting = "No go.");
+  return greeting;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let greeting;
+
+  switch (tip) {
+    case 'generous':
+      greeting = "Thank you so much.";
+      break;
+    case 'not as generous':
+      greeting = "Thank you.";
+      break;
+    default:
+      greeting = "Bye.";
+      break;
+  }
+  return greeting;
 }
